b21a9eec04edd923ec9bec61791012666aa2dfca
shaofa <laishaofa@gmail.com>
2016-01-16 14:50:20 +0800

    nothing

--- update_git.sh
+++ update_git.sh
@@ -1,7 +1,10 @@
 if [ $# -ne 1 ];then
-    echo "parameter error!"
+    echo "*"
+    echo "* Usage: $0  \"log\""
+    echo "*"
     exit
 fi
 sudo git add --all
 sudo git commit -m $1
 sudo git push
+sudo git reflog
